5dea5727d791b3dc7558273ac51787d94ff3d0e5
Karen Suen <karen@Karens-MacBook-Air.local>
2019-03-15 18:15:06 +0000

    added create task function and remigrated contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
   uint public taskCount = 0;  // track total number of tasks in todo list
+
+  struct Task {
+    uint id;
+    string content; // text description of task
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks; // lookup tasks by id
+
+  constructor() public {
+    createTask("Finish this tutorial");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
